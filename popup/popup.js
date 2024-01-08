5733c75eba6370fe6a2aec55717f0ab9234a5dde
Benny <benny88811@gmail.com>
2024-01-08 16:20:36 +0800

    2024/01/08 add single remove

--- popup/popup.js
+++ popup/popup.js
@@ -5,14 +5,12 @@ currentBtn.addEventListener('click', async () => {
     console.log('getCurrent clicked');
     let queryOptions = { active: true, lastFocusedWindow: true };
     let [tab] = await chrome.tabs.query(queryOptions);
-    console.log(tab);
-    if(tab){
-        document.getElementById('inName').value = tab.title
-        document.getElementById('inPrice').value = 0
-        document.getElementById('inURL').value = tab.url
+    if (tab) {
+        document.getElementById('inName').value = tab.title;
+        document.getElementById('inPrice').value = 0;
+        document.getElementById('inURL').value = tab.url;
     }
 });
-
 
 //add Item in List
 let addBtn = document.getElementById('inBtn');
@@ -20,17 +18,19 @@ addBtn.addEventListener('click', function () {
     let inName = document.getElementById('inName').value;
     let inPrice = document.getElementById('inPrice').value;
     let inURL = document.getElementById('inURL').value;
-    itemLists = JSON.parse(localStorage .getItem('itemLists')) || [];
+    itemLists = JSON.parse(localStorage.getItem('itemLists')) || [];
+    // console.log(new Date().getTime())
     itemLists.unshift({
+        id: new Date().getTime(),
         name: inName,
         price: inPrice,
         url: inURL,
     });
-    localStorage .setItem('itemLists', JSON.stringify(itemLists));
+    localStorage.setItem('itemLists', JSON.stringify(itemLists));
     loopList();
-    document.getElementById('inName').value = ''
-    document.getElementById('inPrice').value = 0
-    document.getElementById('inURL').value = ''
+    document.getElementById('inName').value = '';
+    document.getElementById('inPrice').value = 0;
+    document.getElementById('inURL').value = '';
 });
 
 let tableBody = document.getElementById('tableBody');
@@ -47,16 +47,19 @@ let removeAllBtn = document.getElementById('removeAllBtn');
 removeAllBtn.addEventListener('click', () => {
     removeDOMList();
     itemLists = [];
-    localStorage .setItem('itemLists', JSON.stringify(itemLists));
+    localStorage.setItem('itemLists', JSON.stringify(itemLists));
 });
 
 //for loop list
+let removeBtnArr = []
 let loopList = () => {
     removeDOMList();
-    itemLists = JSON.parse(localStorage .getItem('itemLists')) || [];
+    itemLists = JSON.parse(localStorage.getItem('itemLists')) || [];
+    console.log(itemLists);
     itemLists.map((item) => {
         let tr = document.createElement('tr');
         tr.className = 'item';
+        tr.id = item.id;
 
         let tdName = document.createElement('td');
         let tdPrice = document.createElement('td');
@@ -84,7 +87,7 @@ let loopList = () => {
 
         let btn = document.createElement('button');
         btn.innerText = 'Remove';
-        btn.className = 'btn-danger text-xs w-full';
+        btn.className = 'btn-danger text-xs w-full removeBtn';
         tdBtn.appendChild(btn);
 
         tr.appendChild(tdName);
@@ -93,24 +96,36 @@ let loopList = () => {
         tr.appendChild(tdBtn);
         tableBody.appendChild(tr);
     });
+    
+
+    //remove Item in List
+    //因為重新渲染，所以每次都要重新將addEventListener做綁定
+    removeBtnArr = Array.from(document.getElementsByClassName('removeBtn'))
+    removeBtnArr.forEach(removeBtn => {
+        removeBtn.addEventListener('click', () => {
+            let id = removeBtn.parentNode.parentNode.id;
+            itemLists = JSON.parse(localStorage.getItem('itemLists')) || [];
+            itemLists = itemLists.filter((item) => item.id != id);
+            localStorage.setItem('itemLists', JSON.stringify(itemLists));
+            loopList();
+        });
+    });
 };
 loopList();
 
-
-
-//ToThousand(add '，') 
+//ToThousand(add '，')
 const ToThousand = (num) => {
     //拿掉非數字字元(除了負號)
-    num = num.toString().replace(/[^-0-9]/g, '')
+    num = num.toString().replace(/[^-0-9]/g, '');
     //加逗號
-    num = num.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',')
+    num = num.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',');
 
     if (num === '') {
-        return 0
+        return 0;
     } else {
         return num;
     }
-}
+};
 
 let allPrice = tableBody.querySelectorAll('.price');
 allPrice.forEach((price) => {
@@ -118,4 +133,3 @@ allPrice.forEach((price) => {
         price.value = ToThousand(price.value);
     });
 });
-
